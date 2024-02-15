a4b1b5da87461055441754eedfcc2e93f083df6d
André Zambroni <andrezambroni@gmail.com>
2024-02-15 18:17:08 -0300

    POO 2

---
+++ Conta/ContaBancaria.java
@@ -0,0 +1,26 @@
+package Conta;
+
+public class ContaBancaria {
+  String numero;
+  String titular;
+  double saldo;
+
+  // depositar
+  void depositar(double valor) {
+    if (valor > 0) {
+      saldo = saldo + valor;
+      System.out.println("deposito de R$: " + valor + " . Saldo autal: R$ " + saldo);
+    } else {
+      System.out.println("O valor é invalido");
+    }
+  }
+
+  // sacar
+
+  void sacar(double valor) {
+    if (valor > 0 && valor <= saldo) {
+      saldo = saldo - valor;
+      System.out.println("Saque de R$: " + valor + ". Saldo atual: R$ " + saldo);
+    }
+  }
+}
---
+++ Conta/TesteContaBancaria.java
@@ -0,0 +1,17 @@
+package Conta;
+
+public class TesteContaBancaria {
+
+  public static void main(String[] args) {
+    ContaBancaria contaBancaria1 = new ContaBancaria();
+
+    contaBancaria1.numero = "1234";
+    contaBancaria1.titular = "André";
+    contaBancaria1.saldo = 100;
+
+    contaBancaria1.depositar(50);
+
+    contaBancaria1.sacar(100);
+  }
+  
+}
