d8eedb61276791e716b740efd6b6c4f7b27a2d35
André Zambroni <andrezambroni@gmail.com>
2024-02-21 15:18:43 -0300

    inicio SB

---
+++ SB/primeiro-projeto-springboot/src/test/java/br/com/andrezambroni/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrezambroni.primeiroprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
