928e5655ee6789f4d95cf6f09a7782dc695a2a82
André Zambroni <andrezambroni@gmail.com>
2024-06-20 15:22:51 -0300

    20.06

--- SB/sb1/src/main/java/br/com/andrezambroni/controller/Controller1.java
+++ SB/sb1/src/main/java/br/com/andrezambroni/controller/Controller1.java
@@ -1,26 +1,48 @@
 package br.com.andrezambroni.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/Controller1")
 
 public class Controller1 {
 
-  @GetMapping("/primeiroMetodo/{id}")
-  public String primeiroMetodo(@PathVariable String id){
-    return "O parametro é" + id;
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O parametro é" + id;
 
-  }
+    }
 
-  @GetMapping("/metodoComQueryParams")
-  public String metodoComQueryParams(@RequestParam String id) {
-      return "O parametro com metodo query params é: " + id; 
-  }
-  
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O parametro com metodo query params é: " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "O parametro com metodo query params2 é: " + allParams.entrySet();
+    }
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return "metodoComBodyParams" + usuario.username;
+    }
+
+    @PostMapping("/metodoComBodyHeaders")
+    public String metodoComBodyHeaders(@RequestHeader("name") String name) {
+        return "metodoComBodyParams" + usuario.username;
+    }
+
+    record usuario(String username) {
+
+    }
+
 }
